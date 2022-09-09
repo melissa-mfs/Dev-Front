cbcb70b9b4c44a0e934a3344c6bf27eeceadd1b9
Melissa Freitas <melissa.mf29@gmail.com>
2022-09-09 14:22:58 -0300

    calculadora de media e menu do portifolio

---
+++ Calculadora_media/media.js
@@ -0,0 +1,9 @@
+notaPrimeiroBimestre = 5.8764588
+notaSegundoBimestre = 9.2223565342
+notaTerceiroBimestre = 7.3532345
+notaQuartoBimestre = 4.765745
+notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre)/4 
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log(notaFixada)
